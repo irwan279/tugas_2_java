f565c9b966d2ca500b86c63b75a8f6bef5c02195
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 08:46:02 +0700

    tugas 2 java
    Niomic

---
+++ tugas2java.js
@@ -0,0 +1,37 @@
+// variabel number
+var bulat = 20;
+var desimal = 30.5;
+var negatif = -10
+
+// tipe data string
+var huruf = "abcdefghijklmnopqrstupwxyz";
+var tunggal = 'tunggal';
+
+// tipe data boolean
+var tinggi = false;
+var cm = 180;
+
+if (cm>170)
+  tinggi = true;
+else
+    tinggi = false;
+
+// tipe data null
+var kosong = "ada";
+kosong = null;
+
+// type data undifined
+var undifind;
+
+// type data array
+var angka = [3,4,6,7];
+
+console.log(undifind);
+console.log(angka[3]);
+console.log(kosong);
+console.log(bulat);
+console.log(desimal);
+console.log(negatif);
+console.log(huruf);
+console.log(tunggal);
+console.log(tinggi);
